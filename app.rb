62697bcc10796da574f0f2a9fa0a1a1b9f760fe2
Alex Wilson <fonzmp@gmail.com>
2018-06-13 21:17:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -41,10 +41,10 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operator = params[:operation].downcaser
-    if params[:operation].downcase == 'add'
+    @operator = params[:operation].downcase
+    if @operator == 'add'
       "#{params[:num1].to_i + params[:num2].to_i}"
-    elsif params[:operation].downcase
+    elsif @operator.downcase
   end
 
 end
